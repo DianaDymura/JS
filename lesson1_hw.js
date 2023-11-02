e80833c3a7079d4925a95aea88c4ec35ef1304f3
Diana Dymura <didimurkina@gmail.com>
2023-11-02 23:20:31 +0200

    hw_1 commit

---
+++ lesson1_hw.js
@@ -0,0 +1,49 @@
+lesson1_hw.js
+//Task 1
+let ourPlanetName = 'EARTH';
+console.log(ourPlanetName);
+//or
+var our_PlanetName = 'EARTH';
+console.log(our_PlanetName);
+//Task 2
+let current_site_visitor = 'Piter';
+console.log(current_site_visitor);
+//Task 3
+let myAge =  30;
+console.log("My age " + myAge);
+// Task 4
+let myName = 'any name';
+console.log(myName);
+// Task 5
+const myBool = false;
+console.log(myBool);
+//Task 6
+let  x = 15;
+y = 30;
+console.log(x+ y);
+//Task 7
+let myUndefined = undefined;
+console.log(myUndefined);
+//or
+let myUndefined;
+console.log(myUndefined);
+// Task 8
+let myAge = 100;
+dividedNumber = myAge / 2;
+console.log(dividedNumber);
+//Task 9
+let myAge = 65;
+multipleNumber = myAge * 2;
+console.log(multipleNumber);
+//Task 10
+let myName = 'Henrich';
+myAge = 43;
+mySentence = myName +' is '+ myAge;
+console.log(mySentence);
+//Task 11
+let myUndefined2;
+console.log(myUndefined2);
+//Task 12
+let name = "Bill Gates";
+admin = "Bill Gates";
+console.log(admin);
